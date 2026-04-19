5b0a77aa1713a6096c6013b24e2ad77d8fe5b795
Kavya Shah <shahkavya911@gmail.com>
2026-04-19 06:36:57 +0530

    Update voice_test.py

--- voice_test.py
+++ voice_test.py
@@ -1,7 +1,7 @@
 from voice_emotion import start_voice_detection, get_voice_emotion
 import time
 
-print("Voice Emotion Detection Start ho rahi hai...")
+print("Voice Emotion Detection Start ho rahi hai")
 print("Microphone mein bolo - Ctrl+C dabao band karne ke liye\n")
 
 start_voice_detection()
